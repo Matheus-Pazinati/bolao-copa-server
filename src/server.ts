1f44833bd5f2ac22cb5c0cafbadb00767f74b2a9
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-02 05:46:58 -0300

    Cria rota para criar bolao, e valida campo de titulo recebido

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import Fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
 
+import { z } from 'zod'
+
 const prisma = new PrismaClient({
   log: ['query']
 })
@@ -19,6 +21,16 @@ async function bootstrap() {
     const count = await prisma.pool.count()
 
     return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    return { title }
   })
 
   await fastify.listen({ port: 3333 })
